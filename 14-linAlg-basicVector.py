c2e782f4f2c1f8090ee56f8d0ad64b2bf576f3c7
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-12-08 18:44:14 +0700

    [12-08-2024] initial adding readme.md
    update, add several file

---
+++ 14-linAlg-basicVector.py
@@ -0,0 +1,10 @@
+from typing import List, TypeVar
+
+import numpy as np
+from numpy import arccos, dot
+from numpy._typing import NDArray
+
+T = TypeVar("T")
+
+v = np.array([[10, 9, 3]])
+w = np.array([[2, 5, 12]])
